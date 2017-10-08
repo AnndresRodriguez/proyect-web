4cb74a7e688e5bfcb6760211ccec53d63926a6a9
AndrésRodríguez <andresjoe24@gmail.com>
2017-10-08 01:09:56 -0500

    faltan arreglos en mq

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+	$('.carousel').carousel();
+
+	// $(".item_navbar")
+	// .mouseenter(function(){
+
+
+	// 		$(this).css("background-color", "white");
+	// 		$(".item_navbar__link").css("color", "steelblue");
+
+
+
+
+	// }).mouseleave(function() {
+			
+	// 		$(this).css("background-color", "steelblue");
+	// 		$(".item_navbar__link").css("color", "white");
+	// });
+
+
+
+})
